0d4b29d71bc882bd322bf124c08dd0c7f4d48741
max-r-33 <max.rodewald@gmail.com>
2019-03-14 16:19:35 -0700

    basic space algorithm

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import fileinput
 from trie import Node
 
-root = Node('root')
+dict = Node('root')
+searched = {}
 
 def loadDict():
     file = fileinput.input(files=('dictionary.txt')) 
     for l in file:
         line = l.strip()
         if "#!comment" not in line and (len(line) > 1 or line == "a" or  line == "A" or line == "I"):
-            root.add(line)
+            dict.add(line)
     file.close()
 
-# def addSpace(str, result):
-#     length = len(str)
-#     for i in range(1, length+1):
-#         part = str[0:i]
-#         if root.search(part):
-#             if i == length:
-#                 print(part)
-#             # addSpace(str[i:length+1], result + part + " ")
+def addSpace(str):
+    if dict.search(str):
+        return str
+    l = len(str)
+    for i in range(l, 1, -1):
+        start = str[:i]
+        if dict.search(start):
+            ending = str[i:]
+            endWSpaces = addSpace(ending)
+            if endWSpaces != None:
+                print(start + " " + endWSpaces)
+                return start + " " + endWSpaces
 
 def main():
+    # loads dictionary into trie
     loadDict()
-    root.pprint()
+    
+    # reads from input file
     file = open("input.txt", "r")
     inputText = file.readline()
     file.close()
+
+    print("Input: ", inputText)
+    addSpace(inputText)
 
 main()
