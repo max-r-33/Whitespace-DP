70ea53094a3c074fcaead103688ca7462f7adfe1
max-r-33 <max.rodewald@gmail.com>
2019-03-17 16:28:02 -0700

    improving handling of punctuation

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import fileinput
 from trie import Node
 
+punctuation = ['.', '?', '!', ';', ',']
 dict = Node('root')
 searched = {}
 
 def loadDict():
     file = fileinput.input(files=('dictionary.txt')) 
     lineNo = 1
-    wordsToSkip = ['ing', 'en', 'aut', 'de', 'ful', 'uti', 'n\'t', 'goe', 'dra', 'andra']
+    wordsToSkip = ['ing', 'en', 'aut', 'de', 'ful', 'uti', 'n\'t', 'goe', 'dra', 'andra', '\'t','ble','eth', 'et']
     for l in file:
         line = l.strip()
         if "#!comment" not in line and line not in wordsToSkip and (len(line) > 2 or line == "a" or  line == "A" or line == "I" or lineNo <= 200):
@@ -15,23 +16,44 @@ def loadDict():
             dict.add(line)
     file.close()
 
-def extractWords(str, possibleWords):
+# goes through and adds spaces around punctuation
+# to increase accuracy of the program's output
+def preProcess(str):
+    res = []
+    n = len(str)
+    for i, c in enumerate(str):
+        res.append(c)
+        if c in punctuation and i != n -1:
+            res.append(' ')
+    return ''.join(res)
+
+def extractWords(str, possibleWords, savedPunctuation):
     result = ""
     for i, val in enumerate(possibleWords):
         if possibleWords[i] != 0:
             start = i
             end = i + val
             result += str[start:end] + " "
-    return result
+    if savedPunctuation != "":
+        result = result[:-1]
+    return result + savedPunctuation
 
 def insertSpace(str):
-    punctuation = ['.',',']
+    n = len(str)
+    savedPunctuation = ""
+    if str[n-1] in punctuation:
+        savedPunctuation = str[n-1] + " "
+        str = str[:-1]
+    
     n = len(str)
     possibleWords = [0] * n
     usedInidices = [n]
-    i = n-2
+    i = n - 2
     
     while i >= 0:
+        if i in punctuation:
+            # add space after
+            i
         lastUnused = usedInidices[0]
         part = str[i:lastUnused]
         print(part, i, lastUnused)
@@ -43,7 +65,7 @@ def insertSpace(str):
             print(possibleWords)
             print('part', part, 'i',i, 'last unused', lastUnused)
         
-        # if end is reached an first val is 0
+        # if end is reached and first val is 0
         # we need to backtrack and undo the last
         # word we created
         if i == 0 and possibleWords[i] == 0:
@@ -63,8 +85,7 @@ def insertSpace(str):
 
     # when loop is done, get words from the 
     # array of indices of word beginnings
-    return extractWords(str, possibleWords)
-
+    return extractWords(str, possibleWords, savedPunctuation)
             
 def main():
     # loads dictionary into trie
@@ -72,10 +93,12 @@ def main():
     
     # reads from input file
     file = open("input.txt", "r")
-    inputText = file.readline()
+    inputText = preProcess(file.readline()).split(' ')
     file.close()
-
+    res = []
+    for t in inputText:
+        res.append(insertSpace(t))
     print("Input: ", inputText)
-    print(insertSpace(inputText))
+    print(''.join(res))
 
 main()
